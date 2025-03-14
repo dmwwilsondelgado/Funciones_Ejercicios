3db5f899fe56b971cb595428dcce9eb839ca87c3
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2025-03-14 09:43:22 -0500

    develoment

--- app.js
+++ app.js
@@ -1,36 +1,71 @@
-import {getUsuarios,getPost,getCommets,getAlbumm,getPhotos} from "./modulos/index.js";// importamos las funciones de los modulos estas funciones son asincronas
+import {
+  getUsuarios,
+  getPost,
+  getCommets,
+  getAlbumm,
+  getPhotos,
+} from "./modulos/index.js"; // importamos las funciones de los modulos estas funciones son asincronas
 const URL = "https://jsonplaceholder.typicode.com"; // declaramos la url de la api que es una constante para trabajar con ella
-const usuarioId=3;// selecionamos el usuario que queremos buscar con ayuda de baclist 
+const usuarioId = 3; // selecionamos el usuario que queremos buscar con ayuda de baclist
 
-const getusuarioId= async (usuarioId)=>{ // creamos una funcion asincrona que recibe un parametro que es el id del usuario
-    let usuario= await getUsuarios(URL,usuarioId);// declaramos una variable que va a recibir el resultado de la funcion getUsuarios que recibe la url y el id del usuario
-    let post =await getPost(URL,usuario[0]) // declaramos una variable que va a recibir el resultado de la funcion getPost que recibe la url y el usuario
-}// finalizamo la funcion asicorna con un corchete
-getusuarioId(usuarioId);// llamamos a la funcion id y su respectivo parametro que es el id del usuario
+const getusuarioId = async (usuarioId) => {
+  // creamos una funcion asincrona que recibe un parametro que es el id del usuario
+  let usuario = await getUsuarios(URL, usuarioId); // declaramos una variable que va a recibir el resultado de la funcion getUsuarios que recibe la url y el id del usuario
+  let post = await getPost(URL, usuario[0]); // declaramos una variable que va a recibir el resultado de la funcion getPost que recibe la url y el usuario
+}; // finalizamo la funcion asicorna con un corchete
+getusuarioId(usuarioId); // llamamos a la funcion id y su respectivo parametro que es el id del usuario
 
-const manejardatos = async () => { // creamos una funcion asicrona que  no recibe parametros 
-    const usuarios =  await getUsuarios(URL);// declaramos una variable que va a recibir el resultado de la funcion getUsuarios y su respectivo enlace URL
-    return await Promise.all(usuarios.map(async(usuario)=>{ // retornamos una promesa que recibe un arrglo de usuarios y un map que recorre el arreglo de usuarios 
-        const album = await getAlbumm(URL,usuario);// declaramos una variable const que va a recibir el resultado de la funcion getAlbumes que recibe la url y el usuario
-        const getAlbum = await Promise.all(album.map(async(album)=>{
-            const fotos = await getPhotos(URL,album);
-            return {...album,fotos};
-        })); // declaramos una variable const que va a recibir una promesa que recibe un arreglo de albumes y un map que recorre el arreglo de albumes
-        const posts = await getPost(URL,usuario);//declaramos una variable const que va recibir el resultado de la funcion getPost que recibe esta url con el usuario 
-        const comentPost = await Promise.all( posts.map(async(post)=>{ // declaramos una variable que va a recibir una promesa que recibe un arreglo de post y un map que recorre el arreglo de post
-            const coments = await getCommets(URL,post);// declaramos una variable const que va a recibir el resultado de la funcion getComments 
-            return {...post,coments};// retornamo un objeto que va a tener las propiedade de post y coments 
-        })); // ciere
-        return {...usuario,comentPost,getAlbum}; // retornamos un objeto que va a tener las propiedades de usuario y comentPost y album
-    }));// ciere de la promesa retornando un arreglo de usuarios con sus respectivos comentarios
+const manejardatos = async () => {
+  // creamos una funcion asicrona que  no recibe parametros
+  const usuarios = await getUsuarios(URL); // declaramos una variable que va a recibir el resultado de la funcion getUsuarios y su respectivo enlace URL
+  return await Promise.all(
+    usuarios.map(async (usuario) => {
+      const posts = await getPost(URL, usuario); //declaramos una variable const que va recibir el resultado de la funcion getPost que recibe esta url con el usuario
+      const comentPost = await Promise.all(
+        posts.map(async (post) => {
+          // declaramos una variable que va a recibir una promesa que recibe un arreglo de post y un map que recorre el arreglo de post
+          const coments = await getCommets(URL, post); // declaramos una variable const que va a recibir el resultado de la funcion getComments
+          return { ...post, coments }; // retornamo un objeto que va a tener las propiedade de post y coments
+        })
+      ); // ciere
+      return { ...user, albums: albumPhotos, posts: comentPost }; // retornamos un objeto que va a tener las propiedades de usuario y comentPost y album
+    })
+  ); // ciere de la promesa retornando un arreglo de usuarios con sus respectivos comentarios
 };
-manejardatos().then((data)=>{// llamamos a la funcion manejardatos y le pasamos un parametro que es una funcion que recibe un arreglo de datos
-    console.log(data[0]);// mostramos en consola el primer elemento del arreglo de datos
-});// cierre de la funcion manejardatos
+console.log(data);
+manejardatos().then((data) => {
+  // llamamos a la funcion manejardatos y le pasamos un parametro que es una funcion que recibe un arreglo de datos
+  console.log(data[0]); // mostramos en consola el primer elemento del arreglo de datos
+}); // cierre de la funcion manejardatos
 
+const obtenerUsername = async (URL, username) => {
+  const users = await getUsuarios(URL);
+  const user = users.find((user) => user.username === username); // Buscar por username
+  if (!user) {
+    console.log("Usuario no encontrado");
+    return;
+  }
+  const albums = await getAlbum(URL, user.id);
+  const albumPhotos = await Promise.all(
+    albums.map(async (album) => {
+      const photos = await getPhotos(URL, album.id);
+      return { ...album, photos };
+    })
+  );
 
+  console.log({ user, albums: albumPhotos });
+};
 
-const obtengousername = async (URL,usuario) => {
-    
-} // creamos una funcion asincrona que recibe dos parametros URL y usuario
+const filtrarPostsPorNombre = async (URL, nombre) => {
+  const posts = await getPost(URL);
+  const filteredPosts = posts.filter((post) => post.title.includes(nombre));
 
+  const postsWithComments = await Promise.all(
+    filteredPosts.map(async (post) => {
+      const comments = await getComments(URL, post.id);
+      return { ...post, comments };
+    })
+  );
+
+  console.log(postsWithComments);
+};
--- modulos/usuarios.js
+++ modulos/usuarios.js
@@ -1,13 +1,23 @@
 import solicitud from "./solicitud.js";
-export  const getUsuarios=async(URL,id)=>{
-    let ruta="";
-    if(id){
-     ruta=`${URL}/users?id=${id}`;
-    }else{
-        ruta=`${URL}/users`;
-    }
-  
-    const usuarios = await solicitud(ruta);
-    return usuarios;
+export const getUsuarios = async (URL, id) => {
+  let ruta = "";
+  if (id) {
+    ruta = `${URL}/users?id=${id}`;
+  } else {
+    ruta = `${URL}/users`;
+  }
 
-}
+  const usuarios = await solicitud(ruta);
+  return usuarios;
+};
+
+export const getTareasPendientes = async (URL, userId) => {
+  const response = await fetch(`${URL}/todos?userId=${userId}`);
+  const todos = await response.json();
+  return todos.filter((todo) => !todo.completed);
+};
+
+export const getUsersWithPhone = async (URL) => {
+  const users = await getUsuarios(URL);
+  return users.map((user) => ({ name: user.name, phone: user.phone }));
+};
